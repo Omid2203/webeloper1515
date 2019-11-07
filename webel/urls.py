d9514cf53a68322d87e003e03859f7d452f942d2
omid <mhm.mot@gmail.com>
2019-11-07 15:11:57 +0330

    3rd

--- webel/urls.py
+++ webel/urls.py
@@ -21,5 +21,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='base.html'), name='home'),
-    path('signup/', views.signup, name="signup")
+    path('signup', views.signup, name="signup"),
+    path('login', views.loginview, name="login")
 ]
--- webel/views.py
+++ webel/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from .forms import SignUpForm
 
+
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
@@ -16,3 +17,18 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
+
+
+
+def loginview(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect('home')
+    else:
+        messages.error(request, 'username or password not correct')
+        return redirect('login')
+
+    return render(request, 'login.html', {'form': form})
