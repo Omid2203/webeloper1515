a00aab89d9c8754a3e5bf98f7ebf0ee2595f4126
omid <mhm.mot@gmail.com>
2019-11-07 14:37:07 +0330

    3rd commit

--- webel/urls.py
+++ webel/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='base.html'))
+    path('', TemplateView.as_view(template_name='base.html'), name='home'),
+    path('signup/', views.signup, name="signup")
 ]
