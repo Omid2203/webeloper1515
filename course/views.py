5392e0dfb74b8f3f6e44cbdc71d56ea5c6caa4c9
omid <mhm.mot@gmail.com>
2019-11-08 15:01:35 +0330

    #model form

--- course/views.py
+++ course/views.py
@@ -12,26 +12,25 @@ class CourseListView(ListView):
     model = Course
     context_object_name = 'course'
 
-class CourseCreateView(CreateView):
-    template_name = 'courses.html'
-    model = Course
-    form_class = CourseForm
-    success_url = reverse_lazy('courselist')
+# class CourseCreateView(CreateView):
+#     template_name = 'courses.html'
+#     model = Course
+#     form_class = CourseForm
+#     success_url = reverse_lazy('courselist')
 
 def course(request):
     if request.method == 'POST':
         form = CourseForm(request.POST)
         if form.is_valid():
-            department = form.cleaned_data.get('department')
-            name = form.cleaned_data.get('name')
-            course_number = form.cleaned_data.get('course_number')
-            group_number = form.cleaned_data.get('group_number')
-            teacher = form.cleaned_data.get('teacher')
-            start_time = form.cleaned_data.get('start_time')
-            end_time = form.cleaned_data.get('end_time')
-            first_day = form.cleaned_data.get('first_day')
-            second_day = form.cleaned_data.get('second_day')
-
+            # department = form.cleaned_data.get('department')
+            # name = form.cleaned_data.get('name')
+            # course_number = form.cleaned_data.get('course_number')
+            # group_number = form.cleaned_data.get('group_number')
+            # teacher = form.cleaned_data.get('teacher')
+            # start_time = form.cleaned_data.get('start_time')
+            # end_time = form.cleaned_data.get('end_time')
+            # first_day = form.cleaned_data.get('first_day')
+            # second_day = form.cleaned_data.get('second_day')
             form.save()
             return redirect('courselist')
     form = CourseForm()
